b1ed5847bdf2131546962133195c781b99b6e757
arya <aryakhanna@hotmail.com>
2021-08-28 14:00:01 +1000

    installed nodemon and google vision

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
-
-
+const detectText = require('GoogleCloudVision')
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+  detectText().then(t => console.log(t))
 })
 
 app.listen(port, () => {
